4255a091ad5e6b78cdfc2278b80dc043eddd5c06
shreyanshyad <shreyansh.yadav.cse19@itbhu.ac.in>
2022-05-09 03:10:03 +0530

    change token to use 2 filts

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ def init_parser():
     parser.add_argument('--dataset',
                         type=str.lower,
                         choices=['cifar10', 'cifar100'],
-                        default='cifar10')
+                        default='cifar100')
 
     parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
                         help='number of data loading workers (default: 4)')
@@ -103,7 +103,7 @@ def init_parser():
     parser.add_argument('--lr', default=0.002, type=float,
                         # changing lr to 0.0005 to 0.1 for SAM
                         help='initial learning rate')
-    parser.add_argument('--weight-decay', default=3e-2, type=float,
+    parser.add_argument('--weight-decay', default=0.3, type=float,
                         help='weight decay (default: 1e-4)')
 
     return parser

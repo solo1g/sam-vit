278937597692ae9e2bff04f055812125ae1cc109
Shreyansh <56965282+shreyanshyad@users.noreply.github.com>
2022-12-03 10:43:49 +0530

    new

---
+++ helpers.py
@@ -0,0 +1,260 @@
+import random
+from PIL import Image, ImageEnhance, ImageOps
+import torch.nn as nn
+import numpy as np
+import math
+import torch
+import torch.nn.functional as F
+
+# ----------------------------------------------------------------------------------------------
+
+# github:DeepVoltaire/AutoAugment
+
+
+class CIFAR10Policy(object):
+    """ Randomly choose one of the best 25 Sub-policies on CIFAR10.
+
+        Example:
+        >>> policy = CIFAR10Policy()
+        >>> transformed = policy(image)
+
+        Example as a PyTorch Transform:
+        >>> transform=transforms.Compose([
+        >>>     transforms.Resize(256),
+        >>>     CIFAR10Policy(),
+        >>>     transforms.ToTensor()])
+    """
+
+    def __init__(self, fillcolor=(128, 128, 128)):
+        self.policies = [
+            SubPolicy(0.1, "invert", 7, 0.2, "contrast", 6, fillcolor),
+            SubPolicy(0.7, "rotate", 2, 0.3, "translateX", 9, fillcolor),
+            SubPolicy(0.8, "sharpness", 1, 0.9, "sharpness", 3, fillcolor),
+            SubPolicy(0.5, "shearY", 8, 0.7, "translateY", 9, fillcolor),
+            SubPolicy(0.5, "autocontrast", 8, 0.9, "equalize", 2, fillcolor),
+
+            SubPolicy(0.2, "shearY", 7, 0.3, "posterize", 7, fillcolor),
+            SubPolicy(0.4, "color", 3, 0.6, "brightness", 7, fillcolor),
+            SubPolicy(0.3, "sharpness", 9, 0.7, "brightness", 9, fillcolor),
+            SubPolicy(0.6, "equalize", 5, 0.5, "equalize", 1, fillcolor),
+            SubPolicy(0.6, "contrast", 7, 0.6, "sharpness", 5, fillcolor),
+
+            SubPolicy(0.7, "color", 7, 0.5, "translateX", 8, fillcolor),
+            SubPolicy(0.3, "equalize", 7, 0.4, "autocontrast", 8, fillcolor),
+            SubPolicy(0.4, "translateY", 3, 0.2, "sharpness", 6, fillcolor),
+            SubPolicy(0.9, "brightness", 6, 0.2, "color", 8, fillcolor),
+            SubPolicy(0.5, "solarize", 2, 0.0, "invert", 3, fillcolor),
+
+            SubPolicy(0.2, "equalize", 0, 0.6, "autocontrast", 0, fillcolor),
+            SubPolicy(0.2, "equalize", 8, 0.6, "equalize", 4, fillcolor),
+            SubPolicy(0.9, "color", 9, 0.6, "equalize", 6, fillcolor),
+            SubPolicy(0.8, "autocontrast", 4, 0.2, "solarize", 8, fillcolor),
+            SubPolicy(0.1, "brightness", 3, 0.7, "color", 0, fillcolor),
+
+            SubPolicy(0.4, "solarize", 5, 0.9, "autocontrast", 3, fillcolor),
+            SubPolicy(0.9, "translateY", 9, 0.7, "translateY", 9, fillcolor),
+            SubPolicy(0.9, "autocontrast", 2, 0.8, "solarize", 3, fillcolor),
+            SubPolicy(0.8, "equalize", 8, 0.1, "invert", 3, fillcolor),
+            SubPolicy(0.7, "translateY", 9, 0.9, "autocontrast", 1, fillcolor)
+        ]
+
+    def __call__(self, img):
+        policy_idx = random.randint(0, len(self.policies) - 1)
+        return self.policies[policy_idx](img)
+
+    def __repr__(self):
+        return "AutoAugment CIFAR10 Policy"
+
+
+class SubPolicy(object):
+    def __init__(self, p1, operation1, magnitude_idx1, p2, operation2, magnitude_idx2, fillcolor=(128, 128, 128)):
+        ranges = {
+            "shearX": np.linspace(0, 0.3, 10),
+            "shearY": np.linspace(0, 0.3, 10),
+            "translateX": np.linspace(0, 150 / 331, 10),
+            "translateY": np.linspace(0, 150 / 331, 10),
+            "rotate": np.linspace(0, 30, 10),
+            "color": np.linspace(0.0, 0.9, 10),
+            "posterize": np.round(np.linspace(8, 4, 10), 0).astype(np.int),
+            "solarize": np.linspace(256, 0, 10),
+            "contrast": np.linspace(0.0, 0.9, 10),
+            "sharpness": np.linspace(0.0, 0.9, 10),
+            "brightness": np.linspace(0.0, 0.9, 10),
+            "autocontrast": [0] * 10,
+            "equalize": [0] * 10,
+            "invert": [0] * 10
+        }
+
+        func = {
+            "shearX": ShearX(fillcolor=fillcolor),
+            "shearY": ShearY(fillcolor=fillcolor),
+            "translateX": TranslateX(fillcolor=fillcolor),
+            "translateY": TranslateY(fillcolor=fillcolor),
+            "rotate": Rotate(),
+            "color": Color(),
+            "posterize": Posterize(),
+            "solarize": Solarize(),
+            "contrast": Contrast(),
+            "sharpness": Sharpness(),
+            "brightness": Brightness(),
+            "autocontrast": AutoContrast(),
+            "equalize": Equalize(),
+            "invert": Invert()
+        }
+
+        self.p1 = p1
+        self.operation1 = func[operation1]
+        self.magnitude1 = ranges[operation1][magnitude_idx1]
+        self.p2 = p2
+        self.operation2 = func[operation2]
+        self.magnitude2 = ranges[operation2][magnitude_idx2]
+
+    def __call__(self, img):
+        if random.random() < self.p1:
+            img = self.operation1(img, self.magnitude1)
+        if random.random() < self.p2:
+            img = self.operation2(img, self.magnitude2)
+        return img
+
+# -------------------------------------------------------------------------
+
+# github.com:rwightman/pytorch-image-models
+
+
+def drop_path(x, drop_prob: float = 0., training: bool = False):
+    """
+    Obtained from: github.com:rwightman/pytorch-image-models
+    Drop paths (Stochastic Depth) per sample (when applied in main path of residual blocks).
+    This is the same as the DropConnect impl I created for EfficientNet, etc networks, however,
+    the original name is misleading as 'Drop Connect' is a different form of dropout in a separate paper...
+    See discussion: https://github.com/tensorflow/tpu/issues/494#issuecomment-532968956 ... I've opted for
+    changing the layer and argument names to 'drop path' rather than mix DropConnect as a layer name and use
+    'survival rate' as the argument.
+    """
+    if drop_prob == 0. or not training:
+        return x
+    keep_prob = 1 - drop_prob
+    # work with diff dim tensors, not just 2D ConvNets
+    shape = (x.shape[0],) + (1,) * (x.ndim - 1)
+    random_tensor = keep_prob + \
+        torch.rand(shape, dtype=x.dtype, device=x.device)
+    random_tensor.floor_()  # binarize
+    output = x.div(keep_prob) * random_tensor
+    return output
+
+
+class DropPath(nn.Module):
+    """
+    Obtained from: github.com:rwightman/pytorch-image-models
+    Drop paths (Stochastic Depth) per sample  (when applied in main path of residual blocks).
+    """
+
+    def __init__(self, drop_prob=None):
+        super(DropPath, self).__init__()
+        self.drop_prob = drop_prob
+
+    def forward(self, x):
+        return drop_path(x, self.drop_prob, self.training)
+
+# ------------------------------------------------------------------------------------
+
+
+# Credits to DeepVoltaire
+# github:DeepVoltaire/AutoAugment
+
+
+class ShearX(object):
+    def __init__(self, fillcolor=(128, 128, 128)):
+        self.fillcolor = fillcolor
+
+    def __call__(self, x, magnitude):
+        return x.transform(
+            x.size, Image.AFFINE, (1, magnitude *
+                                   random.choice([-1, 1]), 0, 0, 1, 0),
+            Image.BICUBIC, fillcolor=self.fillcolor)
+
+
+class ShearY(object):
+    def __init__(self, fillcolor=(128, 128, 128)):
+        self.fillcolor = fillcolor
+
+    def __call__(self, x, magnitude):
+        return x.transform(
+            x.size, Image.AFFINE, (1, 0, 0, magnitude *
+                                   random.choice([-1, 1]), 1, 0),
+            Image.BICUBIC, fillcolor=self.fillcolor)
+
+
+class TranslateX(object):
+    def __init__(self, fillcolor=(128, 128, 128)):
+        self.fillcolor = fillcolor
+
+    def __call__(self, x, magnitude):
+        return x.transform(
+            x.size, Image.AFFINE, (1, 0, magnitude *
+                                   x.size[0] * random.choice([-1, 1]), 0, 1, 0),
+            fillcolor=self.fillcolor)
+
+
+class TranslateY(object):
+    def __init__(self, fillcolor=(128, 128, 128)):
+        self.fillcolor = fillcolor
+
+    def __call__(self, x, magnitude):
+        return x.transform(
+            x.size, Image.AFFINE, (1, 0, 0, 0, 1, magnitude *
+                                   x.size[1] * random.choice([-1, 1])),
+            fillcolor=self.fillcolor)
+
+
+class Rotate(object):
+    # from https://stackoverflow.com/questions/
+    # 5252170/specify-image-filling-color-when-rotating-in-python-with-pil-and-setting-expand
+    def __call__(self, x, magnitude):
+        rot = x.convert("RGBA").rotate(magnitude)
+        return Image.composite(rot, Image.new("RGBA", rot.size, (128,) * 4), rot).convert(x.mode)
+
+
+class Color(object):
+    def __call__(self, x, magnitude):
+        return ImageEnhance.Color(x).enhance(1 + magnitude * random.choice([-1, 1]))
+
+
+class Posterize(object):
+    def __call__(self, x, magnitude):
+        return ImageOps.posterize(x, magnitude)
+
+
+class Solarize(object):
+    def __call__(self, x, magnitude):
+        return ImageOps.solarize(x, magnitude)
+
+
+class Contrast(object):
+    def __call__(self, x, magnitude):
+        return ImageEnhance.Contrast(x).enhance(1 + magnitude * random.choice([-1, 1]))
+
+
+class Sharpness(object):
+    def __call__(self, x, magnitude):
+        return ImageEnhance.Sharpness(x).enhance(1 + magnitude * random.choice([-1, 1]))
+
+
+class Brightness(object):
+    def __call__(self, x, magnitude):
+        return ImageEnhance.Brightness(x).enhance(1 + magnitude * random.choice([-1, 1]))
+
+
+class AutoContrast(object):
+    def __call__(self, x, magnitude):
+        return ImageOps.autocontrast(x)
+
+
+class Equalize(object):
+    def __call__(self, x, magnitude):
+        return ImageOps.equalize(x)
+
+
+class Invert(object):
+    def __call__(self, x, magnitude):
+        return ImageOps.invert(x)
---
+++ main.py
@@ -0,0 +1,376 @@
+import argparse
+from time import time
+import math
+
+import torch
+import torch.nn as nn
+import torch.optim
+import torch.utils.data
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from helpers import *
+
+
+class LabelSmoothingCrossEntropy(nn.Module):
+    """
+    NLL loss with label smoothing.
+    """
+
+    def __init__(self, smoothing=0.1):
+        """
+        Constructor for the LabelSmoothing module.
+        :param smoothing: label smoothing factor
+        """
+        super(LabelSmoothingCrossEntropy, self).__init__()
+        assert smoothing < 1.0
+        self.smoothing = smoothing
+        self.confidence = 1. - smoothing
+
+    def _compute_losses(self, x, target):
+        log_prob = F.log_softmax(x, dim=-1)
+        nll_loss = -log_prob.gather(dim=-1, index=target.unsqueeze(1))
+        nll_loss = nll_loss.squeeze(1)
+        smooth_loss = -log_prob.mean(dim=-1)
+        loss = self.confidence * nll_loss + self.smoothing * smooth_loss
+        return loss
+
+    def forward(self, x, target):
+        return self._compute_losses(x, target).mean()
+
+
+best_acc1 = 0
+
+epoch_loss = {}
+epoch_loss['train'] = []
+epoch_loss['val'] = []
+epoch_acc = {}
+epoch_acc['train'] = []
+epoch_acc['val'] = []
+epochs_list = []
+
+DATASETS = {
+    'cifar10': {
+        'num_classes': 10,
+        'img_size': 32,
+        'mean': [0.4914, 0.4822, 0.4465],
+        'std': [0.2470, 0.2435, 0.2616]
+    },
+    'cifar100': {
+        'num_classes': 100,
+        'img_size': 32,
+        'mean': [0.5071, 0.4867, 0.4408],
+        'std': [0.2675, 0.2565, 0.2761]
+    }
+}
+
+
+def init_parser():
+    parser = argparse.ArgumentParser(description='CIFAR quick training script')
+
+    # Data args
+    parser.add_argument('data', metavar='DIR',
+                        help='path to dataset', default='./cifar-10-dataset')
+
+    parser.add_argument('--dataset',
+                        type=str.lower,
+                        choices=['cifar10', 'cifar100'],
+                        default='cifar10')
+
+    parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
+                        help='number of data loading workers (default: 4)')
+
+    parser.add_argument('--print-freq', default=10, type=int, metavar='N',
+                        help='log frequency (by iteration)')
+
+    parser.add_argument('--checkpoint-path',
+                        type=str,
+                        default='checkpoint.pth',
+                        help='path to checkpoint (default: checkpoint.pth)')
+
+    # Optimization hyperparams
+    parser.add_argument('--epochs', default=40, type=int, metavar='N',
+                        help='number of total epochs to run')
+    parser.add_argument('--warmup', default=5, type=int, metavar='N',
+                        help='number of warmup epochs')
+    parser.add_argument('-b', '--batch-size', default=128, type=int,
+                        metavar='N',
+                        help='mini-batch size (default: 128)', dest='batch_size')
+    parser.add_argument('--lr', default=3e-4, type=float,
+                        help='initial learning rate')
+    parser.add_argument('--weight-decay', default=1e-4, type=float,
+                        help='weight decay (default: 1e-4)')
+    parser.add_argument('--layers', default=7, type=int,
+                        help='layers (default 7)')
+    parser.add_argument('--dim', default=256, type=int,
+                        help='embedding dim (default 256)')
+    parser.add_argument('--heads', default=4, type=int,
+                        help='heads num (4)')
+    parser.add_argument('--mlp', default=2, type=float,
+                        help='mlp ratio (2)')
+
+    return parser
+
+
+def main():
+    global best_acc1
+    global epochs_list
+
+    parser = init_parser()
+    args = parser.parse_args()
+    img_size = DATASETS[args.dataset]['img_size']
+    num_classes = DATASETS[args.dataset]['num_classes']
+    img_mean, img_std = DATASETS[args.dataset]['mean'], DATASETS[args.dataset]['std']
+
+    from model3 import ShiftingTransformer
+    model = ShiftingTransformer(
+        scaling_factor=4,
+        output_dir='/Users/one/work/Locally-SAG-Transformer/sam-vit',
+        num_variants=18,
+        image_size=32,
+        patch_size=1,
+        embedding_dim=((192, 192), (192, 192), (192, 192), (192, 192)),
+        heads=3,
+        num_hierarchies=4,  # number of hierarchies
+        # the number of transformer blocks at each heirarchy, starting from the bottom
+        num_layers_per_block=(2, 4, 4, 4),
+        num_classes=10,
+        init_patch_embed_size=1,
+        kernel_size=3,
+        stride_size=1,
+        padding_size=1
+    )
+
+    criterion = LabelSmoothingCrossEntropy()
+
+    epochs_list = [i+1 for i in range(args.epochs)]
+
+    if torch.cuda.is_available():
+        torch.cuda.set_device(0)
+        model.cuda(0)
+        criterion = criterion.cuda(0)
+
+    optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr,
+                                  weight_decay=args.weight_decay)
+
+    # base_optimizer = torch.optim.AdamW
+    # from sam import SAM
+    # optimizer = SAM(model.parameters(), base_optimizer,
+    #                 lr=args.lr, weight_decay=args.weight_decay, adaptive=True, rho=0.4)
+
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+        optimizer, T_max=args.epochs, eta_min=0)
+
+    normalize = [transforms.Normalize(mean=img_mean, std=img_std)]
+
+    augmentations = [CIFAR10Policy()]
+    augmentations += [
+        transforms.RandomCrop(img_size, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        *normalize,
+    ]
+
+    augmentations = transforms.Compose(augmentations)
+    train_dataset = datasets.__dict__[args.dataset.upper()](
+        root=args.data, train=True, download=True, transform=augmentations)
+
+    val_dataset = datasets.__dict__[args.dataset.upper()](
+        root=args.data, train=False, download=False, transform=transforms.Compose([
+            transforms.Resize(img_size),
+            transforms.ToTensor(),
+            *normalize,
+        ]))
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=args.batch_size, shuffle=True,
+        num_workers=args.workers)
+
+    val_loader = torch.utils.data.DataLoader(
+        val_dataset,
+        batch_size=args.batch_size, shuffle=False,
+        num_workers=args.workers)
+
+    print("Beginning training")
+    print("Epochs: ", args.epochs)
+    print("LR: ", args.lr)
+    print("Layers: ", args.layers)
+    print("Embedding dim: ", args.dim)
+    print("Heads: ", args.heads)
+    print("MLP: ", args.mlp)
+    print("Dataset: ", args.dataset)
+    time_begin = time()
+    for epoch in range(args.epochs):
+
+        # adjust_learning_rate(optimizer, epoch, args)
+        cls_train(train_loader, model, criterion, optimizer, epoch, args)
+        acc1 = cls_validate(val_loader, model, criterion,
+                            args, epoch=epoch, time_begin=time_begin)
+        best_acc1 = max(acc1, best_acc1)
+        scheduler.step()
+
+    total_mins = (time() - time_begin) / 60
+    print(f'Script finished in {total_mins:.2f} minutes, '
+          f'best top-1: {best_acc1:.2f}, '
+          f'final top-1: {acc1:.2f}')
+    torch.save(model.state_dict(), args.checkpoint_path)
+
+    import matplotlib.pyplot as plt
+    fig, ax = plt.subplots()
+    ax.plot(epochs_list, epoch_loss['train'], label='train')
+    ax.plot(epochs_list, epoch_loss['val'], label='val')
+    ax.set_title("Loss Epoch Graph")
+    ax.set_xlabel("Epoch")
+    ax.set_ylabel("Loss")
+    ax.legend()
+
+    fig.savefig('./epoch_loss.png')
+    fig.show()
+    # -----------------------------------
+    fig, ax = plt.subplots()
+    ax.plot(epochs_list, epoch_acc['train'], label='train')
+    ax.plot(epochs_list, epoch_acc['val'], label='val')
+    ax.set_title("Accuracy Epoch Graph")
+    ax.set_xlabel("Epoch")
+    ax.set_ylabel("Accuracy")
+    ax.legend()
+
+    fig.savefig('./epoch_acc.png')
+    fig.show()
+
+
+def adjust_learning_rate(optimizer, epoch, args):
+    lr = args.lr
+    if epoch >= 20 and epoch < 30:
+        lr = 0.001
+    elif epoch >= 30 and epoch < 35:
+        lr = 0.0006
+    elif epoch >= 35:
+        lr = 0.0003
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+    return None
+
+    tot_epochs = args.epochs
+    LR = args.lr
+    stepsize = 2
+    k_min = 0.1
+    k_max = 0.08
+
+    N_MIN = LR*0.9
+    N_MAX = LR
+
+    n_min = N_MIN
+    n_max = N_MAX
+
+    lr = LR
+
+    for epoch in range(1, tot_epochs+1):
+        warmup = args.warmup
+        if epoch <= warmup:
+            lr = lr
+        else:
+            ep = epoch-warmup
+            n_max = N_MAX*math.exp(-ep*k_max)
+            n_min = N_MIN*math.exp(-ep*k_min)
+            cycle = 1+ep//(2*stepsize)
+            x = abs(ep/stepsize-2*cycle+1)
+            lr = n_min+(n_max-n_min)*max(0, 1-x)
+
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+def accuracy(output, target):
+    with torch.no_grad():
+        batch_size = target.size(0)
+
+        _, pred = output.topk(1, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        correct_k = correct[:1].flatten().float().sum(0, keepdim=True)
+        res.append(correct_k.mul_(100.0 / batch_size))
+        return res
+
+
+def cls_train(train_loader, model, criterion, optimizer, epoch, args):
+    model.train()
+    loss_val, acc1_val = 0, 0
+    n = 0
+    for i, (images, target) in enumerate(train_loader):
+        if torch.cuda.is_available():
+            images = images.cuda(0, non_blocking=True)
+            target = target.cuda(0, non_blocking=True)
+
+        def closure():
+            loss = criterion(model(images), target)
+            loss.backward()
+            return loss
+
+        output = model(images)
+
+        loss = criterion(output, target)
+
+        acc1 = accuracy(output, target)
+        n += images.size(0)
+        loss_val += float(loss.item() * images.size(0))
+        acc1_val += float(acc1[0] * images.size(0))
+
+        loss.backward()
+        optimizer.step(closure)
+        optimizer.zero_grad()
+
+        # if args.clip_grad_norm > 0:
+        #     nn.utils.clip_grad_norm_(
+        #         model.parameters(), max_norm=args.clip_grad_norm, norm_type=2)
+
+        if args.print_freq >= 0 and i % args.print_freq == 0:
+            avg_loss, avg_acc1 = (loss_val / n), (acc1_val / n)
+            print(
+                f'[Epoch {epoch + 1}][Train][{i}] \t Loss: {avg_loss:.4e} \t Top-1 {avg_acc1:6.2f}')
+
+    epoch_loss['train'].append(loss_val/n)
+    epoch_acc['train'].append(acc1_val/n)
+
+
+def cls_validate(val_loader, model, criterion, args, epoch=None, time_begin=None):
+    model.eval()
+    loss_val, acc1_val = 0, 0
+    n = 0
+    with torch.no_grad():
+        for i, (images, target) in enumerate(val_loader):
+            if torch.cuda.is_available():
+                images = images.cuda(0, non_blocking=True)
+                target = target.cuda(0, non_blocking=True)
+
+            output = model(images)
+            loss = criterion(output, target)
+
+            acc1 = accuracy(output, target)
+            n += images.size(0)
+            loss_val += float(loss.item() * images.size(0))
+            acc1_val += float(acc1[0] * images.size(0))
+
+            if args.print_freq >= 0 and i % args.print_freq == 0:
+                avg_loss, avg_acc1 = (loss_val / n), (acc1_val / n)
+                print(
+                    f'[Epoch {epoch + 1}][Eval][{i}] \t Loss: {avg_loss:.4e} \t Top-1 {avg_acc1:6.2f}')
+
+    avg_loss, avg_acc1 = (loss_val / n), (acc1_val / n)
+    epoch_loss['val'].append(avg_loss)
+    epoch_acc['val'].append(avg_acc1)
+    total_mins = -1 if time_begin is None else (time() - time_begin) / 60
+    print(
+        f'[Epoch {epoch + 1}] \t \t Top-1 {avg_acc1:6.2f} \t \t Time: {total_mins:.2f}')
+
+    return avg_acc1
+
+
+if __name__ == '__main__':
+    main()
